84277cfb5dbdf48671644110367cb0ece546a66d
jgoliszewski <jakub.goliszewski01@gmail.com>
2022-12-16 21:50:26 +0100

    keycpade hotfix

--- keyboards/crkbd/keymaps/jgol/keymap.c
+++ keyboards/crkbd/keymaps/jgol/keymap.c
@@ -106,7 +106,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       ╰─────────┴─────────┴─────────╯  ╰─────────┴─────────┴─────────╯ */ 
   [_ADJUST] = LAYOUT_split_3x6_3(
 // ╷         ╷         ╷         ╷         ╷         ╷         ╷            ╷         ╷         ╷         ╷         ╷         ╷         ╷
-    RESET,    _______,  _______,  _______,  _______,  _______,               _______,  KC_F7,    KC_F8,    KC_F9,    KC_F12,   RESET,
+    QK_BOOT,  _______,  _______,  _______,  _______,  _______,               _______,  KC_F7,    KC_F8,    KC_F9,    KC_F12,   QK_BOOT,
     RGB_TOG,  RGB_HUI,  RGB_SAI,  RGB_VAI,  _______,  _______,               _______,  KC_F4,    KC_F5,    KC_F6,    KC_F11,   _______,
     RGB_MOD,  RGB_HUD,  RGB_SAD,  RGB_VAD,  _______,  _______,               _______,  KC_F1,    KC_F2,    KC_F3,    KC_F10,   _______,
 //                                    ╷         ╷         ╷         ╷  ╷         ╷         ╷         ╷             
